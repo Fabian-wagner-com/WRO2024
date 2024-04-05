361ad16a2e4d629536d7991cc816861024cfc518
Fabi <fabian.wagner1909@web.de>
2024-04-05 17:32:32 +0200

    4 Rote Steine abstellen, kleinen Schrott weg bringen, erste Wasserleitung Reparieren

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from ev3dev2.sensor import INPUT_3, INPUT_2
 from ev3dev2.sensor.lego import ColorSensor
 
 spkr = Sound()
-color_sensor = ColorSensor(INPUT_3)
+#color_sensor = ColorSensor(INPUT_3)
 line_sensor = ColorSensor(INPUT_2)
 move_motors = MoveSteering(OUTPUT_B, OUTPUT_C)
 
@@ -23,21 +23,32 @@ def ausparken():
     drive_for_cm.drive(10, 3.1)
     turn_for_degrees.turn(15, 44, "right")
     drive_for_cm.drive(-10, 9.1)
-    turn_for_degrees.turn(-15, 12, "right")
+    turn_for_degrees.turn(-15, 10.5, "right")
+
+
+def ausparken2():
+    #ausparken
+    turn_for_degrees.turn(-15, 40, "left")
+    turn_for_degrees.turn(15, 20, "right")
+    drive_for_cm.drive(10, 3.1)
+    turn_for_degrees.turn(15, 44, "right")
+    drive_for_cm.drive(-10, 8.5)
+    turn_for_degrees.turn(-15, 11.98, "right")
+
 
 def absetzen():
     #absetzen
     motor_ele.on_for_rotations(50, 0.35)
     motor_zang.on_for_rotations(-50, 1)
-    motor_ele.on_for_rotations(-50, 0.62)
-    drive_for_cm.drive(3, 10)
+    motor_ele.on_for_rotations(-50, 0.63)
+    drive_for_cm.drive(3, 8.4)
 
 
 
 #variabeln
 #-------------------------------
 
-#Abstand zu den Banden. Fareben bedeuten da, wo die Startbereiche den Farben am nächstens sin.
+#Abstand zu den Banden. Fareben bedeuten da, wo die Startbereiche den Farben am nächstens sind.
 wall_distance_green = 0 # in cm
 wall_distance_red = 1 #in cm
 
@@ -60,6 +71,7 @@ spkr.speak('S')
 
 wait_for_button.wait()
 time.sleep(0.6)
+start_time = time.time()
 
 motor_ele.on_for_rotations(-50, fahrstulbewegung)
 
@@ -87,36 +99,20 @@ motor_ele.on_for_rotations(50, fahrstulbewegung)
 motor_zang.on_for_rotations(50, 1.2)
 motor_ele.on_for_rotations(-50, fahrstulbewegung)
 
-#zu blau/grün Steinen fahren
-turn_for_degrees.turn(35, 90, "right")
-drive_for_cm.drive(30, 4.6)
-turn_on_spot_for_degrees.turn(30, 88)
-drive_for_cm.drive(-25, 46)
-#farbe erkennen
-stein1 = color_sensor.color_name
-spkr.speak(stein1)
-print(stein1)
-
-drive_for_cm.drive(-10, 9.8)
-stein2 = color_sensor.color_name
-spkr.speak(stein2)
-print(stein2)
 
 #rüber fahren
-turn_for_degrees.turn(30, 85, "right")
-drive_for_cm.drive(45, 40)
-turn_on_spot_for_degrees.turn(-25, 30)
-drive_for_cm.drive(30, 24)
-turn_on_spot_for_degrees.turn(25, 27)
-drive_for_cm.drive(30, 24)
+turn_for_degrees.turn(35, 127, "right")
+drive_for_cm.drive(65, 83)
+turn_for_degrees.turn(35, 36, "left")
+drive_for_cm.drive(35, 25.4)
+time.sleep(0.2)
+drive_for_cm.drive(-10, wall_distance_red)
 
-drive_for_cm.drive(-10, wall_distance_red + 0.45)
+ausparken2()
 
-ausparken()
-
-#nach hinten fahren bis zu weiß
+#nach hinten fahren bis zu braun
 reflection = line_sensor.reflected_light_intensity
-if reflection <= 50:
+if reflection <= 55:
     move_motors.on(0, 10)
     while reflection <= 50:
         reflection = line_sensor.reflected_light_intensity
@@ -143,8 +139,46 @@ motor_zang.on_for_rotations(50, 1.2)
 motor_ele.on_for_rotations(-50, fahrstulbewegung)
 
 #zum abstellen
-turn_for_degrees.turn(35, 90, "right")
-drive_for_cm.drive(30, 42)
+turn_for_degrees.turn(35, 88, "right")
+drive_for_cm.drive(30, 43.2)
 turn_on_spot_for_degrees.turn(20, 90)
-drive_for_cm.drive(30, 16)
+drive_for_cm.drive(30, 16.6)
 absetzen()
+
+#Schrott in d'Ecke bringen
+turn_for_degrees.turn(40, 46, "left")
+drive_for_cm.drive(55, 50)
+
+#zur Wasserleitung rasen
+turn_for_degrees.turn(-40, 44, "right")
+drive_for_cm.drive(-45, 17)
+turn_for_degrees.turn(-40, 90, "right")
+motor_ele.on_for_rotations(60, 0.47)
+drive_for_cm.drive(-45, 17.3)
+
+#Klempner spielen  --> Wasserleitung reparerien
+motor_ele.on_for_rotations(-60, 0.47)
+turn_on_spot_for_degrees.turn(-35,90)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+spkr.speak('Yeah')
+
+#Zeit ausgeben
+#-------------------------------
+end_time = time.time()
+duration = round(end_time - start_time, 2)
+print( duration, "Sekunden")
